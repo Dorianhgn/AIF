c7275be22051e451ab5fbacab8f7bf47d2589d3a
Dorianhgn <dorianhugonnet1@gmail.com>
2025-09-29 18:02:32 +0200

    TP1: Tout avant docker

--- developpement/MNIST/mnist_webapp.py
+++ developpement/MNIST/mnist_webapp.py
@@ -7,10 +7,19 @@ import io
 def recognize_digit(image):
     # Convert to PIL Image necessary if using the API method
     image = Image.fromarray(image.astype('uint8'))
-    img_binary = ...
-    ...
-    ...
-    return ....
+    img_binary = io.BytesIO()
+    image.save(img_binary, format='PNG')
+    img_binary = img_binary.getvalue()
+
+    response = requests.post("http://127.0.0.1:5000/predict", data=img_binary, headers={"Content-Type": "application/octet-stream"})
+    if response.ok:
+        prediction = response.json().get("prediction", -1)
+    else:
+        print(f"Request failed with status {response.status_code} - {response.reason}")
+        print("Response body:", response.text)
+        prediction = -1
+
+    return prediction
 
 if __name__=='__main__':
 
